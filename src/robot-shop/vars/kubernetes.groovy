128fe09c00f9bfcb5a6de2f0fa839ae1b787e79c
Raghava <rajuvishnu2003@gmail.com>
2024-04-05 12:41:19 -0400

    Added KubeConfig

--- src/robot-shop/vars/kubernetes.groovy
+++ src/robot-shop/vars/kubernetes.groovy
@@ -1,10 +1,18 @@
 def call() {
-   stage('Kubernetes status') {
-      steps {
-         //sh 'kubectl version'
-         //sh 'kubectl get nodes'
-	 sh 'ls -lhrt'
-	 sh 'date'
-      }
-   }
+    stage('Kubernetes') {
+        steps {
+            script {
+                // Define the kubeconfig path
+                def kubeconfigPath = '/root/.kube/config'
+
+                // Use withKubeConfig step to define kubeconfig
+                withKubeConfig(kubeconfigPath) {
+                    // Your Kubernetes-related steps here
+                    sh 'kubectl get nodes'
+		    sh 'kubectl get svc -n robot-shop'
+                }
+            }
+        }
+    }
 }
+
